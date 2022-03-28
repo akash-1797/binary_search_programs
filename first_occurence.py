699021eb0b8f24e6f1794dccf642134b6287c0fc
Akash Singh <akash1510013@gmail.com>
2022-03-28 10:57:13 +0530

    first occurence in sorted array

---
+++ first_occurence.py
@@ -0,0 +1,20 @@
+def binary_search(arr,key):
+  res = 'not found'
+  low = 0
+  high = len(arr)-1
+  while(low <= high):
+    mid = low + (high-low)//2
+    if arr[mid] == key:
+      res =  mid
+      high = mid - 1
+    elif arr[mid] > key:
+      high = mid -1
+    else:
+        low = mid +1
+  return res 
+
+arr = [12,32,32,32,44,56,79]
+key = 32 
+print(binary_search(arr,key))
+
+
